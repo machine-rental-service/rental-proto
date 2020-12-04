c9850ff04486b3cbd82741bdd80c80ce0dd7bd83
melonSoda <sodamlee315@gmail.com>
2020-12-04 14:25:08 +0900

    Spring boot2 + jsp 세팅

---
+++ rental/src/main/java/com/hackaton/prize/contoller/HomeController.java
@@ -0,0 +1,14 @@
+package com.hackaton.prize.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {//테스트용 컨트롤러입니다
+
+    @RequestMapping("/home/main")
+    public String showMain() {
+        return "main"; //jsp 폴더 이하의.jsp 파일을 기재해주시면됩니다.
+    }
+
+}
