39c9834033b0b4331d72889948313bef18f57ca1
sodam <66295123+sodaMelon@users.noreply.github.com>
2020-12-07 23:28:33 +0900

    Feature/21/set home(메인페이지 및 GNB 세팅) (#22)
    * [일반인모드]대여 조회
    
    * [일반인모드]대여 조회 - rentalCheckList 수정
    
    * 대여조회
    
    * 대여 작성후 , 해당 건 조회 연결
    
    * [일반인모드]대여 조회 - rentalCheckDetail.jsp 수정
    
    * 기본 메인화면과 gnb 세팅
    
    Co-authored-by: subin <soobin1080@naver.com>

--- rental/src/main/java/com/hackaton/prize/contoller/HomeController.java
+++ rental/src/main/java/com/hackaton/prize/contoller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {//테스트용 컨트롤러입니다(변경금지)
 
-  @RequestMapping("/test")
+  @RequestMapping("/")
     public String showMain() {
         return "main"; //jsp 폴더 이하의.jsp 파일을 기재해주시면됩니다.
     }
