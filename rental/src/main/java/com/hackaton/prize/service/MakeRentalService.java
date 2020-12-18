9e4d36953b3f1cc0fc404dce9f21a2fb755696b9
melonSoda <sodamlee315@gmail.com>
2020-12-18 10:58:26 +0900

    heroku서버시간 자기맘대로 읽어오는거 fix

--- rental/src/main/java/com/hackaton/prize/service/MakeRentalService.java
+++ rental/src/main/java/com/hackaton/prize/service/MakeRentalService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 @Service
@@ -41,6 +42,9 @@ public class MakeRentalService {
     }
 
     public String merge(RentalDetailDto detailDto, RentalDto rentalDto) {
+        LocalDateTime servertime = LocalDateTime.now();
+        int currentHour = servertime.getHour();
+
         Rental rental = Rental.builder()
                 .lesseeName(rentalDto.getLesseeName())
                 .lesseeBirthday(rentalDto.getLesseeBirthday())
@@ -49,9 +53,15 @@ public class MakeRentalService {
                 .lesseeAddress(rentalDto.getLesseeAddress())
                 .localInstitution(rentalDto.getLocalInstitution())
                 .started(detailDto.getStarted())
-                .applied(LocalDate.now(ZoneId.of("Asia/Seoul")))
+                .applied(LocalDateTime.now().toLocalDate())
                 .deadline(detailDto.getStarted().plusDays(3))
                 .status(defaultStatus).build();
+
+        if (currentHour < 14) { //14시간 시차라, 13시 59분이전일때 하루늦게 처리되는 localDate 이슈
+            rental.setApplied(rental.getApplied().plusDays(1));//접수일
+            rental.setStarted(rental.getStarted().plusDays(1));//희망예정일
+            rental.setDeadline(rental.getDeadline().plusDays(1));//마감일
+        }
 
         RentalDetail detail = RentalDetail.builder()
                 .rental(rental)
@@ -64,7 +74,7 @@ public class MakeRentalService {
                 .ricePlantingMachine(detailDto.getRicePlantingMachine())
                 .riceHarvester(detailDto.getRiceHarvester())
                 .otherToolsRequest(detailDto.getOtherToolsRequest())
-                .staffComment(whiteSpace).build();
+                .staffComment(LocalDateTime.now().toString()).build();
 
         rental.setRentalDetail(detail);
         rentalRepository.save(rental);
